a351e59189e564e69220859138d8aaede04d3d5c
Frank Rose <frankrosework@gmail.com>
2018-03-29 02:35:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,29 +5,29 @@ var destructivelyAppendKitten = function(name) {
 }
 
 var destructivelyPrependKitten = function(name) {
-  
+
 }
 
 var destructivelyRemoveLastKitten = function() {
-  
+
 }
 
 var destructivelyRemoveFirstKitten = function() {
-  
+
 }
 
 var appendKitten = function(name) {
-  
+
 }
 
 var prependKitten = function(name) {
-  
+
 }
 
 var removeLastKitten = function() {
-  
+
 }
 
 var removeFirstKitten = function() {
-  
+
 }
