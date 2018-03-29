fc89516396ed910d80e6aa404ae3442a5245e99c
Frank Rose <frankrosework@gmail.com>
2018-03-29 02:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const app = "I don't do much."
 
 var destructivelyAppendKitten = function(name) {
+
+}
+
+var destructivelyPrependKitten = function(name) {
+  
+}
+
+var destructivelyRemoveLastKitten = function() {
+  
+}
+
+var destructivelyRemoveFirstKitten = function() {
+  
+}
+
+var appendKitten = function(name) {
+  
+}
+
+var prependKitten = function(name) {
+  
+}
+
+var removeLastKitten = function() {
+  
+}
+
+var removeFirstKitten = function() {
   
 }
